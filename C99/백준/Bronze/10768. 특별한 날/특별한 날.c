3c504647308bc12932c51ede8f64db11ee4b3036
kimwoosoung <162290228+kimwoosoung@users.noreply.github.com>
2024-05-08 23:34:25 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Bronze/10768. 특별한 날/특별한 날.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int m, d;
+    scanf("%d %d", &m, &d);
+    if (m < 2)
+        printf("Before");
+    else if (m == 2){
+        if (d < 18)
+            printf("Before");
+        else if (d > 18)
+            printf("After");
+        else
+            printf("Special");
+    }
+    else
+        printf("After");
+    return 0;
+}
