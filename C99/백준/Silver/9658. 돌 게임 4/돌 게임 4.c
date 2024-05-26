bcfe64da902f4d7ff8440e6b1258a3c66c9d9b1e
kimwoosoung <162290228+kimwoosoung@users.noreply.github.com>
2024-05-26 23:53:37 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Silver/9658. 돌 게임 4/돌 게임 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%7 == 1 || a%7 == 3){
+        printf("CY");
+    }
+    else{
+        printf("SK");
+    }
+    return 0;
+}
