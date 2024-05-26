016210f18403f7fb880ed6b6d4b53080c2dbbe22
kimwoosoung <162290228+kimwoosoung@users.noreply.github.com>
2024-05-26 23:21:40 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C99/백준/Silver/9657. 돌 게임 3/돌 게임 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a%7 == 0 || a%7 == 2){
+        printf("CY");
+    }
+    else{
+        printf("SK");
+    }
+    return 0;
+}
