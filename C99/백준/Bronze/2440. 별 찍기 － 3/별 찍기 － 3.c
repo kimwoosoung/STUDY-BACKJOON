9385f8bc634d3516e3cdc9782728bf7b30d23290
kimwoosoung <162290228+kimwoosoung@users.noreply.github.com>
2024-11-12 23:05:25 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
+++ C99/백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int num;
-    scanf("%d",&num);
-    for(int i=1; i<=num; i++){
-        for(int j=i; j<=num; j++){
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < i; j++){
             printf("*");
         }
-        printf("\n");
-    }
-
-    return 0;
+		printf("\n");
+	}
+	return 0;
 }
