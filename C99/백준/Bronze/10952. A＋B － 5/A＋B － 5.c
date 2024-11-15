0cc263b2694d051b95d22993cffd79a1570c0367
kimwoosoung <162290228+kimwoosoung@users.noreply.github.com>
2024-11-15 20:32:09 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- C99/백준/Bronze/10952. A＋B － 5/A＋B － 5.c
+++ C99/백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int a,b;
-    while(1){
-        scanf("%d %d",&a,&b);
-        if(a == 0 && b == 0)break;
-        printf("%d",a+b);
-        printf("\n");
-    }
+	int a, b;
+	
+	while(1) {
+		scanf("%d %d", &a, &b);
+		if(a == 0 && b == 0) break;
+		printf("%d \n", a + b);
+	}
 
-    return 0;
+	return 0;
 }
