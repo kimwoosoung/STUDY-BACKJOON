b9b7980af5eb22d8142c78fdd27bf638a25a54d7
kimwoosoung <162290228+kimwoosoung@users.noreply.github.com>
2025-04-12 22:57:36 +0900

    [Bronze II] Title: 진수 변환기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/14915. 진수 변환기/진수 변환기.py
@@ -0,0 +1,9 @@
+a,b =map(int,input().strip().split())
+
+def conversion (a,b) :
+    c="0123456789ABCDEF"
+    if a<b:
+        return str(c[a])
+    else:
+        return conversion (a//b,b)+str(c[a%b])
+print (conversion(a,b))
